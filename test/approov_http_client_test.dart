f6a1b7eb053d2e483ec7a4d55ea44f56146b756f
Umair Adil <umair.adil@northbaysolutions.net>
2023-04-06 22:02:39 +0500

    - Added web support

--- test/approov_http_client_test.dart
+++ test/approov_http_client_test.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/services.dart';
 import 'package:flutter_test/flutter_test.dart';
-import 'package:approov_http_client/approov_http_client.dart';
+//import 'package:approov_http_client/approov_http_client.dart';
 
 void main() {
   const MethodChannel channel = MethodChannel('approov_http_client');
